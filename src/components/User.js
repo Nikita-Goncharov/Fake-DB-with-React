c8f0ccefa700581b68dbd452b6b7bfda7aef9091
Nikita-Goncharov <nikitagoncarov657@gmail.com>
2023-01-02 09:30:07 +0200

    change user printing structure

---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class User extends React.Component {
+    user = this.props.user
+    render() {
+        return (
+            <div className='user'>
+                <h3>{this.user.firstname} {this.user.lastname}</h3>
+                <p>{this.user.bio}</p>
+                <b>{this.user.isHappy ? 'Happy :)' : 'Sad :('}</b>
+            </div>
+        )
+    }
+}
+
+export default User
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,35 +1,36 @@
 import React from "react"
-
+import User from "./User"
 
 class Users extends React.Component {
-    users = [
-        {
-            id: 1,
-            firstname: 'Nik',
-            lastname: 'Goncharov',
-            age: 16,
-            bio: 'ojnafbeaiubet;ibeoub',
-            isHappy: true
-        },
-        {
-            id: 2,
-            firstname: 'Bob',
-            lastname: 'Smith',
-            age: 44,
-            bio: 'ojnafbeaiuaffdbdbbet;ibeoub',
-            isHappy: false
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Nik',
+                    lastname: 'Goncharov',
+                    age: 16,
+                    bio: 'ojnafbeaiubet;ibeoub',
+                    isHappy: true
+                },
+                {
+                    id: 2,
+                    firstname: 'Bob',
+                    lastname: 'Smith',
+                    age: 44,
+                    bio: 'ojnafbeaiuaffdbdbbet;ibeoub',
+                    isHappy: false
+                }
+            ]
         }
-    ]
+    }
     render() {
-        if (this.users.length > 0) {
+        if (this.state.users.length > 0) {
             return (
                 <div>
-                    {this.users.map((user) => (
-                        <div className='user' key={user.id}>
-                            <h3>{user.firstname} {user.lastname}</h3>
-                            <p>{user.bio}</p>
-                            <b>{user.isHappy ? 'Happy :)' : 'Sad :('}</b>
-                        </div>
+                    {this.state.users.map((user) => (
+                        <User key={user.id} user={user} />
                     ))}
                 </div>
             )
