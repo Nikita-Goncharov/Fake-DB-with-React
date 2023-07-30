0c8029caa9b4076bdd8ed8066bac29aec8165aba
Nikita-Goncharov <nikitagoncarov657@gmail.com>
2023-07-30 18:16:04 +0300

    refactor & change readme file

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -24,37 +24,37 @@ class AddUser extends React.Component {
                 <label htmlFor='isHappy'>Are you happy ?</label>
                 <input type='checkbox' id='isHappy' onChange={event => this.setState({ isHappy: event.target.checked })} />
                 <button type='button' onClick={() => {
-                    this.myForm.reset()
-                    
-                    if(this.props.isNewUser) {
-                        this.userAdd = {
-                            firstname: this.state.firstname,
-                            lastname: this.state.lastname,
-                            age: this.state.age,
-                            bio: this.state.bio,
-                            isHappy: this.state.isHappy,
+                        this.myForm.reset()
+                        
+                        if(this.props.isNewUser) {
+                            this.userAdd = {
+                                firstname: this.state.firstname,
+                                lastname: this.state.lastname,
+                                age: this.state.age,
+                                bio: this.state.bio,
+                                isHappy: this.state.isHappy,
+                            }
+                        } else {
+                            this.userAdd = {
+                                firstname: this.state.firstname || this.props.user.firstname,
+                                lastname: this.state.lastname || this.props.user.lastname,
+                                age: this.state.age || this.props.user.age,
+                                bio: this.state.bio || this.props.user.bio,
+                                isHappy: this.state.isHappy,
+                            }
                         }
-                    } else {
-                        this.userAdd = {
-                            firstname: this.state.firstname || this.props.user.firstname,
-                            lastname: this.state.lastname || this.props.user.lastname,
-                            age: this.state.age || this.props.user.age,
-                            bio: this.state.bio || this.props.user.bio,
-                            isHappy: this.state.isHappy,
+                        
+                        if (this.props.user) {
+                            this.userAdd.id = this.props.user.id
+                        }
+                        this.setState({ firstname: '', lastname: '', age: 0, bio: '', isHappy: false})
+                        for(const i in this.state) {
+                            if (Boolean(this.state[i]) === true) {
+                                this.props.onAdd(this.userAdd)
+                                break
+                            }
                         }
                     }
-                    
-                    if (this.props.user) {
-                        this.userAdd.id = this.props.user.id
-                    }
-                    this.setState({ firstname: '', lastname: '', age: 0, bio: '', isHappy: false})
-                    for(const i in this.state) {
-                        if (Boolean(this.state[i]) === true) {
-                            this.props.onAdd(this.userAdd)
-                            break;
-                        }
-                    }
-                }
                 }>Add</button>
             </form>
         )
